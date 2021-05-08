39c9de7e5cd2ad9055394fd235ab41ef68484eef
Lucian Lazar <lucian@mainquest.ro>
2021-05-08 23:47:50 +0300

    updated repo url

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='py_runner',
     version='0.0.4',
     description="Utils for running other processes or shell commands. " +
-                "'Install by 'pip install --upgrade git+https://github.com/tfgstudios/py_runner.git#egg=py_runner'",
-    url='https://github.com/tfgstudios/py_runner.git',
+                "'Install by 'pip install --upgrade git+https://github.com/main-quest/py_runner.git#egg=py_runner'",
+    url='https://github.com/main-quest/py_runner.git',
     author='The Fallen Games',
     author_email='contact@thefallengames.com',
     license='unlicense',
