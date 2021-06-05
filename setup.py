d2c1b1938f7d913d6979e3f73562fcb8f284a4b2
Lucian Lazar <lucian@mainquest.ro>
2021-06-05 23:21:40 +0300

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='py_runner',
-    version='0.0.7',
+    version='0.0.8',
     description="Utils for running other processes or shell commands. " +
                 "'Install by 'pip install --upgrade git+https://github.com/main-quest/py_runner.git#egg=py_runner'",
     url='https://github.com/main-quest/py_runner.git',
